c4428309001dc3003f0ee7b6dce1f9b8ed195dec
iaw26068632 <iaw26068632@h18.informatica.escoladeltreball.org>
2017-09-20 16:33:46 +0200

    Primeros metodos

--- src/org/escoladeltreball/helloworld/Main.java
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -3,11 +3,13 @@
  */
 package org.escoladeltreball.helloworld;
 
+import java.util.Arrays;
+
 /**
  * @author iaw26068632
  *
  */
-public final class Main {
+public final class Main implements Utils {
 
 	/**
 	 * @param args
@@ -15,7 +17,59 @@ public final class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World");
+		int[] valores = { 9, 3, 8, 1, 2};
+		Main main = new Main();
+		
+		System.out.println("El valor mínimo es: " + main.findSmallest(valores));
+		System.out.println("La suma de valores es: " + main.sum(valores));
 
+	}
+/*
+ * para obtener el esqueleto del metodo de la interficie ctrl + 1
+ * 
+ * 
+ * (non-Javadoc)
+ * Stub
+ * this methods returns the smallest value from values
+ * @see org.escoladeltreball.helloworld.Utils#findSmallest(int[])
+ */
+	@Override
+	public int findSmallest(int[] values) {
+		// TODO Auto-generated method stub
+		/*
+		 * OPCION B
+		 * int temp = values[0];
+		 * for (int value : values) {
+		 * if ( value < temp) {
+		 * temp = value;
+		 * 	}
+		 * }
+		 * return temp;
+		 */
+		int smallest;
+		if (values.length > 0) {
+			Arrays.sort(values);
+			smallest = values[0];
+		} else {
+			smallest = 0;
+		}
+		return smallest;
+	}
+	/*
+	 * (non-Javadoc)
+	 * this method returns the sum of values
+	 * @see org.escoladeltreball.helloworld.Utils#sum(int[])
+	 */
+	@Override
+	public int sum(int[] values) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		if (values.length > 0) {
+			for (int value: values) {
+				sum += value;
+			}
+		}
+		return sum;
 	}
 
 }
---
+++ src/org/escoladeltreball/helloworld/Utils.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author iaw26068632
+ * 20.09.2017
+ */
+public interface Utils {
+
+	/*
+	 * this method returns the smalllest value in a integer array
+	 */
+	public abstract int findSmallest(int[] values);
+	
+	/*
+	 * this method returns the sum of values
+	 */
+	public abstract int sum(int[] values);
+	
+}
